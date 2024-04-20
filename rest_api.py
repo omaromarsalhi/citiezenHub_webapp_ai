9f2b8e93bdf047bf96bab0855b913f3bcfa0b883
omar salhi <omar.salhi@esprit.tn>
2024-04-20 22:48:45 +0100

    adding the title and category test

---
+++ rest_api.py
@@ -0,0 +1,43 @@
+from flask import Flask,request,jsonify
+from langchain_helper import generate_produxt_description
+import ollama_vesion as ov
+
+
+
+app = Flask(__name__)
+url='https://ea25-34-122-33-80.ngrok-free.app'
+
+@app.route('/get-descreption',methods=['POST'])
+def generate():
+    text=generate_produxt_description(request.args.get('title'))
+    return jsonify(text),200
+
+@app.route('/get-product_image_descreption',methods=['POST'])
+def generate_desc_image():
+    image_url=request.args.get('image_url')
+    commands=f"set OLLAMA_HOST={url}&ollama run llava describe the image content only {image_url}"
+    text=ov.execute_cmd(commands)
+    tuned_response=ov.finetune_text(text)
+    return jsonify(tuned_response),200
+
+
+@app.route('/get-title_validation',methods=['POST'])
+def generate_title_validation():
+    desc=request.args.get('desc')
+    title=request.args.get('title')
+    text=ov.generateBasedOnText(desc,title,url)
+    tuned_response=ov.finetune_resp(text)
+    return jsonify(tuned_response),200
+
+@app.route('/get-category_validation',methods=['POST'])
+def generate_category_validation():
+    desc=request.args.get('desc')
+    title=request.args.get('category')
+    text=ov.generateBasedOnCategory(desc,title,url)
+    tuned_response=ov.finetune_resp(text)
+    return jsonify(tuned_response),200
+
+
+
+if __name__== '__main__':
+    app.run()

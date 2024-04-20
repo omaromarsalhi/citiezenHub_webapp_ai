5351657fb3ba7d2aecb6b875d93d1db64fdb2841
omar salhi <omar.salhi@esprit.tn>
2024-04-20 22:51:39 +0100

    adding the title and category test

--- ollama_vesion.py
+++ ollama_vesion.py
@@ -2,13 +2,6 @@ import json
 import requests
 import subprocess
 
-<<<<<<< HEAD
-
-# GITIGNORE
-# url='https://ab7e-104-155-185-76.ngrok-free.app'
-=======
-url='https://ab7e-104-155-185-76.ngrok-free.app'
->>>>>>> 86902ae1e004de4f29fc654de3782bdc98fa4227
 
 
 def execute_cmd(command):
@@ -19,7 +12,6 @@ def execute_cmd(command):
     except Exception as e:
         print(f"Error executing command: {e}")
 
-<<<<<<< HEAD
 def generateBasedOnText(prompt,prompt2,url):
     r = requests.post(url+'/api/generate',
                     json={
@@ -46,34 +38,11 @@ def generateBasedOnCategory(prompt,prompt2,url):
 # image_url="E:\\usersImg\\0e4525b5-2aa0-469f-9aa6-a36ea7765481.png"
 # commands=f"set OLLAMA_HOST={url}&ollama run llava only describe the image content  {image_url}"
 # response=execute_cmd(commands)
-=======
-def generateBasedOnText(prompt,prompt2):
-    r = requests.post(url+'/api/generate',
-                    json={
-                        'model': 'mistral',
-                        'prompt': 'is this paragraph '+prompt+' speaks about '+prompt2+' please answer me with yes or no ',
-                    },
-                    stream=True)
-    r.raise_for_status()
-    for line in r.iter_lines():
-        body = json.loads(line)
-        response_part = body.get('response', '')
-        # the response streams one token at a time, print that as we recieve it
-        print(response_part, end='', flush=True)
-
-        if 'error' in body:
-            raise Exception(body['error'])
-
-        if body.get('done', False):
-            return body['context']
 
 
 
 
-image_url="E:\\usersImg\\0e4525b5-2aa0-469f-9aa6-a36ea7765481.png"
-commands=f"set OLLAMA_HOST={url}&ollama run llava only describe the image content  {image_url}"
-response=execute_cmd(commands)
->>>>>>> 86902ae1e004de4f29fc654de3782bdc98fa4227
+
 
 
 def finetune_text(text):
@@ -83,7 +52,6 @@ def finetune_text(text):
             tuned_response+=string
     return tuned_response
 
-<<<<<<< HEAD
 
 def finetune_resp(text):
     newResp=text.split('\n')
@@ -94,7 +62,5 @@ def finetune_resp(text):
         final_response+=body.get('response', '')
     return final_response
 
-=======
->>>>>>> 86902ae1e004de4f29fc654de3782bdc98fa4227
 # print(tuned_response)
 # generateBasedOnText(tuned_response,"Golf")
